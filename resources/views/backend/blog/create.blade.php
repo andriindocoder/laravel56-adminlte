679b557bb483a22cebfc0b7da7580e25546483b8
Andri Wicaksono <andriwicaksonost@gmail.com>
2018-09-16 22:29:17 +0700

    Form Modification - Wordpress-Like Blog Laravel 5.7 and AdminLTE 3 (12)

--- resources/views/backend/blog/create.blade.php
+++ resources/views/backend/blog/create.blade.php
@@ -25,7 +25,13 @@
 	    <section class="content">
 	      <div class="container-fluid">
 	        <div class="row">
-	          <div class="col-md-12">
+	          <div class="col-md-9">
+	          	{!! Form::model($post, [
+	          		'method' => 'POST',
+	          		'route' => 'backend.blog.store',
+	          		'files' => TRUE,
+	          		'id' 	=> 'post-form'
+	          	])!!}
 	            <div class="card">
 	              <!-- /.card-header -->
 	              <div class="card-header">
@@ -44,12 +50,6 @@
 
 	            <!-- /.card-header -->
 				<div class="card-body p-1">
-					{!! Form::model($post, [
-						'method' => 'POST',
-						'route' => 'backend.blog.store',
-						'files' => TRUE,
-					])!!}
-
 				<div class="form-group {{ $errors->has('title') ? 'has-error' : ''}}">
 					{!! Form::label('title') !!}
 					{!! Form::text('title', null, ['class'=>'form-control']) !!}
@@ -66,7 +66,7 @@
 					@endif
 				</div>
 
-				<div class="form-group {{ $errors->has('excerpt') ? 'has-error' : ''}}">
+				<div class="form-group excerpt {{ $errors->has('excerpt') ? 'has-error' : ''}}">
 					{!! Form::label('excerpt') !!}
 					{!! Form::textarea('excerpt', null, ['class'=>'form-control']) !!}
 					@if($errors->has('excerpt'))
@@ -81,38 +81,6 @@
 						<span class="badge badge-danger">{{ $errors->first('body') }}</span>
 					@endif
 				</div>
-
-				<div class="form-group {{ $errors->has('published_at') ? 'has-error' : ''}}">
-					{!! Form::label('published_at', 'Publish Date') !!}
-					{!! Form::text('published_at', null, ['class'=>'form-control','placeholder' => 'Date Format: Y-m-d H:i:s']) !!}
-					@if($errors->has('published_at'))
-						<span class="badge badge-danger">{{ $errors->first('published_at') }}</span>
-					@endif
-				</div>
-
-				<div class="form-group {{ $errors->has('category_id') ? 'has-error' : ''}}">
-					{!! Form::label('category_id', 'Category') !!}
-					{!! Form::select('category_id', App\Category::pluck('title','id'), null, ['class'=>'form-control', 'placeholder' => 'Select Category']) !!}
-					@if($errors->has('category_id'))
-						<span class="badge badge-danger">{{ $errors->first('category_id') }}</span>
-					@endif
-				</div>
-
-				<div class="form-group {{ $errors->has('image') ? 'has-error' : ''}}">
-					{!! Form::label('image', 'Featured Image') !!}
-					<br>
-					{!! Form::file('image') !!}
-
-					@if($errors->has('image'))
-						<span class="badge badge-danger">{{ $errors->first('image') }}</span>
-					@endif
-				</div>
-
-				<hr>
-
-				{!! Form::submit('Create New Post', ['class' => 'btn btn-primary']) !!}
-
-					{!! Form::close() !!}
 				</div>
 				<div class="card-footer clearfix">
 
@@ -121,7 +89,84 @@
         </div>
         <!-- /.card -->
       </div>
+      <div class="col-md-3">
+          <div class="card card-default">
+            <div class="card-header">
+              <h3 class="card-title"> Publish</h3>
+            </div>
+            <div class="card-body">
+              <div class="form-group">
+                  <div class="form-group {{ $errors->has('published_at') ? 'has-error' : ''}}">
+				  {!! Form::label('published_at','Published Date') !!}
 
+				  <div class='input-group date'>
+                      {!! Form::text('published_at', null, ['class'=> 'form-control','placeholder' => 'Y-m-d H:i:s', 'id'=>'datetimepicker1']) !!}
+                      <span class="input-group-addon">
+                          <span class="glyphicon glyphicon-calendar"></span>
+                      </span>
+                  </div>
+				    
+
+				  @if($errors->has('published_at'))
+				  <span class="label label-danger help-block">{{ $errors->first('published_at') }}</span>
+				  @endif
+				</div>
+              </div>
+            </div>
+            <div class="card-footer">
+                <div class="pull-right">
+                  {!! Form::submit('Publish', ['class' => 'btn btn-primary']) !!}
+                </div>
+                <div class="pull-left">
+                	<a id="draft-btn" href="#" class="btn btn-default">Save Draft</a>
+                </div>
+            </div>
+          </div>
+          <!-- /.card -->
+          <div class="card card-default">
+            <div class="card-header">
+              <h3 class="card-title"> Category</h3>
+            </div>
+            <div class="card-body">
+				<div class="form-group {{ $errors->has('category_id') ? 'has-error' : ''}}">
+					{!! Form::select('category_id', App\Category::pluck('title','id'), null, ['class'=>'form-control', 'placeholder' => 'Select Category']) !!}
+					@if($errors->has('category_id'))
+						<span class="badge badge-danger">{{ $errors->first('category_id') }}</span>
+					@endif
+				</div>
+            </div>
+          </div>
+          <div class="card card-default">
+            <div class="card-header with-border">
+              <h3 class="card-title"> Featured Image</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body text-center">
+              <div class="form-group {{ $errors->has('image') ? 'has-error' : ''}}">
+              	<div class="fileinput fileinput-new" data-provides="fileinput">
+              	  <div class="fileinput-new thumbnail" style="width: 200px; height: 150px;">
+              	    <img src="http://placehold.it/200x150&text=No+Image" alt="...">
+              	  </div>
+              	  <div class="fileinput-preview fileinput-exists thumbnail" style="max-width: 200px; max-height: 150px;"></div>
+              	  <div>
+              	    <span class="btn btn-default btn-file"><span class="fileinput-new">Select image</span><span class="fileinput-exists">Change</span>
+              	    {!! Form::file('image') !!}
+              		</span>
+              	    <a href="#" class="btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
+              	  </div>
+              	</div>
+
+              	@if($errors->has('image'))
+              		<span class="badge badge-danger">{{ $errors->first('image') }}</span>
+              	@endif
+              </div>
+            </div>
+              <!-- /.card-body -->
+          </div>
+      </div>
+      <!-- right column -->
+		{!! Form::close() !!} 
+	  </div>
     </div>
     <!-- /.row -->
   </div><!-- /.container-fluid -->
@@ -134,5 +179,25 @@
 @section('script')
 	<script type="text/javascript">
 		$('#pagination').addClass('no-margin pagination-sm');
+
+		$('#title').on('blur',function(){
+			var theTitle = this.value.toLowerCase().trim(),
+				slugInput = $('#slug');
+				theSlug = theTitle.replace(/&/g, '-and')
+								.replace(/[^a-z0-9-]+/g,'-')
+								.replace(/\-\-+/g, '-')
+								.replace(/^-+|-+$/g, '');
+
+			slugInput.val(theSlug);
+		});
+
+		var simplemde1 = new SimpleMDE({ element: $("#excerpt")[0] });
+		var simplemde2 = new SimpleMDE({ element: $("#body")[0] });
+
+		$('#draft-btn').on('click',function(e){
+			e.preventDefault();
+			$('#published_at').val("");
+			$('#post-form').submit();
+		})
 	</script>
 @endsection
--- resources/views/layouts/backend/main.blade.php
+++ resources/views/layouts/backend/main.blade.php
@@ -24,6 +24,13 @@
   <link rel="stylesheet" href="{{ asset('backend/plugins/daterangepicker/daterangepicker-bs3.css') }}">
   <!-- bootstrap wysihtml5 - text editor -->
   <link rel="stylesheet" href="{{ asset('backend/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
+
+  <link rel="stylesheet" href="{{ asset('backend/plugins/simplemde/simplemde.min.css') }}">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/css/jasny-bootstrap.min.css">
+  
+  <link rel="stylesheet" href="{{ asset('backend/css/custom.css') }}">
+
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
@@ -89,7 +96,9 @@
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="{{ asset('backend/dist/js/pages/dashboard.js') }}"></script>
 <!-- AdminLTE for demo purposes -->
+<script src="{{ asset('backend/plugins/simplemde/simplemde.min.js') }}"></script>
 <script src="{{ asset('backend/dist/js/demo.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/js/jasny-bootstrap.min.js"></script>
 
 @yield('script')
 </body>
